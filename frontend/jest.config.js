2eb7e36c14d204b3ef76871b109f932ab83e8c46
Roman-e-prog <roman.rostock@googlemail.com>
2024-08-28 14:48:43 +0200

    configured jest

---
+++ frontend/jest.config.js
@@ -0,0 +1,19 @@
+
+const config = {
+    verbose:true,
+    transform: {
+        "^.+\\.jsx$": "babel-jest", 
+        "^.+\\.js$": "babel-jest", 
+    },
+    testEnvironment: 'jest-environment-jsdom',
+    moduleNameMapper: {
+        "\\.(css|less|scss|sass)$": "identity-obj-proxy" //for css imports jest not handles out of the box
+      },
+      setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+      moduleDirectories: ['node_modules', '<rootDir>/', 'src'],
+      collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
+      moduleFileExtensions: ['js', 'mjs', 'jsx'],
+      clearMocks: true,
+};
+
+module.exports = config;
