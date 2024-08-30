1d05ddd2fc0c17645c23d041c8aed2df3a03e003
Roman-e-prog <roman.rostock@googlemail.com>
2024-08-30 09:31:14 +0200

    cleaned up

--- backend/router/todosRouter.js
+++ backend/router/todosRouter.js
@@ -1,6 +1,5 @@
 const todosRouter = require('express').Router();
 const todosController = require('../controller/todosController');
-console.log(todosController, 'hier Daten')
 const {createTodo, updateTodo, deleteTodo, getTodo, getAllTodo} = todosController;
 todosRouter.post('/', createTodo );
 todosRouter.put('/:id', updateTodo );
