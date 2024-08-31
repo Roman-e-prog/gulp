8482b1ed45dc4ebe9b6458a6aaf319ddc1cf70c2
Roman-e-prog <roman.rostock@googlemail.com>
2024-08-31 12:11:52 +0200

    destructured src and dest from gulp

--- gulp-tasks/scripts.js
+++ gulp-tasks/scripts.js
@@ -1,14 +1,14 @@
-const gulp = require('gulp');
+const {src, dest} = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 
 function scripts() {
-  return gulp.src('frontend/src/**/*.jsx') // Target all JSX files in the frontend/src directory
+  return src('frontend/src/**/*.jsx') // Target all JSX files in the frontend/src directory
     .pipe(babel({
       presets: ['@babel/preset-react']
     }))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js')); // Output the compiled and minified JS to the dist/js directory
+    .pipe(dest('dist/js')); // Output the compiled and minified JS to the dist/js directory
 }
 
 module.exports = scripts;
--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -1,12 +1,12 @@
-const gulp = require('gulp');
+const {src, dest} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 
 function styles() {
-  return gulp.src('frontend/src/**/*.scss') // This targets all SCSS files in the frontend/src directory
+  return src('frontend/src/**/*.scss') // This targets all SCSS files in the frontend/src directory
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS({ compatibility: 'ie8' })) // Minify the CSS
-    .pipe(gulp.dest('dist/css')); // Output the compiled and minified CSS to the dist/css directory
+    .pipe(dest('dist/css')); // Output the compiled and minified CSS to the dist/css directory
 }
 
 module.exports = styles;
