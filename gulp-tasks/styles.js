23752f24671defca1d9cdc36616990abff36556a
Roman-e-prog <roman.rostock@googlemail.com>
2024-09-12 08:52:15 +0200

    added

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -6,7 +6,7 @@ function styles() {
   return src('frontend/src/**/*.scss') // This targets all SCSS files in the frontend/src directory
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS({ compatibility: 'ie8' })) // Minify the CSS
-    .pipe(dest('dist/css')); // Output the compiled and minified CSS to the dist/css directory
+    .pipe(dest('frontend/build/css')); // Output the compiled and minified CSS to the dist/css directory
 }
 
 module.exports = styles;
