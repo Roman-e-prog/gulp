c60897bbc9a3fdfa1d0e75258c007e3d0ed58cbb
Roman-e-prog <roman.rostock@googlemail.com>
2024-09-12 08:47:06 +0200

    changed in env variables

--- backend/db/dbConnect.js
+++ backend/db/dbConnect.js
@@ -3,14 +3,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-    user: 'postgres',
-    host: process.env.DB_HOST || 'postgres', //for docker not the localhost but the the servicename
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST || 'localhost', //for docker not the localhost but the the servicename
     database: 'todo_gulp',
     password: process.env.DATABASE_PASSWORD,
     port: process.env.PORT
 });
 
-console.log(pool);
 pool.query('SELECT NOW()', (err, res) => {
     if (err) {
         console.log('Connection to Database failed', err);
