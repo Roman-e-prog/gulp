29ebf619f518d098f4182aa4bd8d70c3e9c2627e
Roman-e-prog <roman.rostock@googlemail.com>
2024-09-12 08:52:04 +0200

    added

--- gulp-tasks/scripts.js
+++ gulp-tasks/scripts.js
@@ -1,14 +1,14 @@
-const {src, dest} = require('gulp');
+const { src, dest } = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 
 function scripts() {
   return src('frontend/src/**/*.jsx') // Target all JSX files in the frontend/src directory
     .pipe(babel({
-      presets: ['@babel/preset-react']
+      presets: ['@babel/preset-env', '@babel/preset-react'],
     }))
     .pipe(uglify())
-    .pipe(dest('dist/js')); // Output the compiled and minified JS to the dist/js directory
+    .pipe(dest('frontend/build/js')); // Output the compiled and minified JS to the dist/js directory
 }
 
 module.exports = scripts;
