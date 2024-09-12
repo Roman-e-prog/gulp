470f73b22776c28395aba55a5e57d28de9a419a7
Roman-e-prog <roman.rostock@googlemail.com>
2024-09-12 08:49:26 +0200

    added

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -3,4 +3,10 @@ module.exports = {
       '@babel/preset-env',
       '@babel/preset-react',
     ],
+    // "plugins": [
+    //   "@babel/plugin-syntax-export-default-from",
+    //   "@babel/plugin-proposal-export-default-from",
+    //   ["@babel/plugin-proposal-decorators", {"legacy": true}],
+    //   "@babel/plugin-proposal-class-properties"
+    // ]
   };
