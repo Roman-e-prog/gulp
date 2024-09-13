7be2a769bad4298a8dc36f2ccbdf599eff1a3654
Roman-e-prog <roman.rostock@googlemail.com>
2024-09-13 15:04:00 +0200

    log the envs

--- backend/db/dbConnect.js
+++ backend/db/dbConnect.js
@@ -1,7 +1,10 @@
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
-
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_NAME:', process.env.DB_NAME);
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST, //for docker not the localhost but the the servicename
