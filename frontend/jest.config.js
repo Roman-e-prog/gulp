770933c8e2aea7a3ad60d9610f3d60e6091d7d2e
Roman-e-prog <roman.rostock@googlemail.com>
2024-09-17 11:43:52 +0200

    configured for seperating playwright

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,22 +1,22 @@
+
 const config = {
-    verbose: true,
+    verbose:true,
     transform: {
-        "^.+\\.jsx$": "babel-jest",
-        "^.+\\.js$": "babel-jest",
+        "^.+\\.jsx$": "babel-jest", 
+        "^.+\\.js$": "babel-jest", 
     },
     testEnvironment: 'jest-environment-jsdom',
     moduleNameMapper: {
-        "\\.(css|less|scss|sass)$": "identity-obj-proxy"
-    },
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-    moduleDirectories: ['node_modules', '<rootDir>/', 'src'],
-    collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
-    moduleFileExtensions: ['js', 'mjs', 'jsx'],
-    clearMocks: true,
-    testMatch: ['<rootDir>/frontend/src/**/*.test.jsx'], // Use source directory
-    transformIgnorePatterns: [
-        "/node_modules/"
-    ],
+        "\\.(css|less|scss|sass)$": "identity-obj-proxy" //for css imports jest not handles out of the box
+      },
+      setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+      moduleDirectories: ['node_modules', '<rootDir>/', 'src'],
+      collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
+      moduleFileExtensions: ['js', 'mjs', 'jsx'],
+      clearMocks: true,
+      testMatch: ['<rootDir>/frontend/src/**/*.test.jsx'],
+      testPathIgnorePatterns: ['/node_modules/', '/playwrightTests/', '/test-results/', 'playwright-report'],
+      modulePathIgnorePatterns: ['**/*.spec.js'], 
 };
 
 module.exports = config;
